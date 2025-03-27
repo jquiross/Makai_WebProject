76d2cec33d67baf9a87f84c2555fe4621610c24b
Esteban <estebanleivaaguilar@gmail.com>
2025-03-26 23:29:14 -0600

    Login
    Registar Cuenta e Iniciar Sesion

---
+++ Makai_APIProject/Controllers/LoginController.cs
@@ -0,0 +1,103 @@
+﻿using Dapper;
+using Makai_APIProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Makai_APIProject.Controllers
+{
+   
+
+        [AllowAnonymous]
+        [Route("api/[controller]")]
+        [ApiController]
+        public class LoginController : ControllerBase
+        {
+            private readonly IConfiguration _configuration;
+            public LoginController(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+
+            [HttpPost]
+            [Route("RegistrarCuenta")]
+            public IActionResult RegistrarCuenta(UsersModel model)
+            {
+                using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:BDConnection").Value))
+                {
+                    var result = context.Execute("RegistrarCuenta",
+                        new { model.email, model.name, model.password, model.address ,model.phone,model.role_id});
+
+                    var respuesta = new RespuestaModel();
+
+                    if (result > 0)
+                        respuesta.Indicador = true;
+                    else
+                    {
+                        respuesta.Indicador = false;
+                        respuesta.Mensaje = "Su información no se ha registrado correctamente";
+                    }
+
+                    return Ok(respuesta);
+                }
+            }
+
+            [HttpPost]
+            [Route("IniciarSesion")]
+            public IActionResult IniciarSesion(UsersModel model)
+            {
+                using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:BDConnection").Value))
+                {
+                    var result = context.QueryFirstOrDefault<UsersModel>("IniciarSesion",
+                        new { model.email, model.password });
+
+                    var respuesta = new RespuestaModel();
+
+                    if (result != null)
+                    {
+                        result.Token = GenerarToken(result.user_id, result.role_id);
+
+                        respuesta.Indicador = true;
+                        respuesta.Datos = result;
+                    }
+                    else
+                    {
+                        respuesta.Indicador = false;
+                        respuesta.Mensaje = "Su información no se ha validado correctamente";
+                    }
+
+                    return Ok(respuesta);
+                }
+            }
+
+            //Recuperar la contraseña
+
+            private string GenerarToken(long user_id,long role_id)
+            {
+                string SecretKey = _configuration.GetSection("Variables:llaveToken").Value!;
+
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim("Id", user_id.ToString()));
+                claims.Add(new Claim("Role", role_id.ToString()));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+                var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+                var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(20),
+                    signingCredentials: cred);
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+
+        }
+
+    }
+        
+
---
+++ Makai_APIProject/Models/UsersModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Makai_APIProject.Models
+{
+    public class UsersModel
+    {
+
+        public long user_id { get; set; }
+        public string? name { get; set; }
+        public string? password { get; set; }
+        public string? email { get; set; }
+
+        public string? phone { get; set; }
+
+        public string? address { get; set; }
+        public long role_id { get; set; }
+        public string? Token { get; set; }
+    }
+}
+

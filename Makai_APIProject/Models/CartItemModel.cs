479a05dc3ea7d4ba81f56bde8ee07995496596dc
Jeremy Quirós <117528758+jquiross@users.noreply.github.com>
2025-04-09 18:09:47 -0600

    cart
    cart

--- Makai_APIProject/Models/CartItemModel.cs
+++ Makai_APIProject/Models/CartItemModel.cs
@@ -5,6 +5,5 @@
         public int user_id { get; set; }
         public int product_id { get; set; }
         public int quantity { get; set; }
-        public DateTime added_at { get; set; }
     }
 }
